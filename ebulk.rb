6f10f738942d0f664b7a2cbf5df0fd22b1802653
roqueporchetto@gmail.com <roqueporchetto@gmail.com>
2018-11-27 14:52:56 +0100

    ebulk release v1.1.1

--- ebulk.rb
+++ ebulk.rb
@@ -1,8 +1,8 @@
 class Ebulk < Formula
   desc "Ingestion/download of large dataset into Wendelin-IA-Telecom platform"
   homepage "https://lab.nexedi.com/rporchetto/ebulk"
-  url "https://lab.nexedi.com/rporchetto/ebulk/repository/archive.tar.gz?ref=v1.1.0"
-  sha256 "21eb04a255cffd0dbcf1b996741424a548e7f3df4ce094229e0fc32934ffb77a"
+  url "https://lab.nexedi.com/rporchetto/ebulk/repository/archive.tar.gz?ref=v1.1.1"
+  sha256 "7ef33f88488e5bb454ba39069acc82f43c5b6d648f9be69bba3c23c76f614622"
 
   bottle :unneeded
 
