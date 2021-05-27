43fbfda521f96d93886109637402c314dc9903f7
Roque Porchetto <roque.porchetto@nexedi.com>
2021-05-27 18:41:47 +0200

    Release 1.1.9

--- ebulk.rb
+++ ebulk.rb
@@ -1,8 +1,8 @@
 class Ebulk < Formula
   desc "Ingestion/download of large dataset into Wendelin-IO platform"
   homepage "https://lab.nexedi.com/nexedi/ebulk"
-  url "https://lab.nexedi.com/nexedi/ebulk/repository/archive.tar.gz?ref=v1.1.5"
-  sha256 "002e1164d41c6cdfeb061544b445706a37394f59c459b718fe4c84f1284e1d04"
+  url "https://lab.nexedi.com/nexedi/ebulk/repository/archive.tar.gz?ref=v1.1.9"
+  sha256 "4ad14b259914ab9e71ca220c980a84c406b8c127dccd802ed4830b001da9ae2c"
 
   bottle :unneeded
 
